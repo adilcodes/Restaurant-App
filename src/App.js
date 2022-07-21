f3d5c1678548c6a2dc9aeb59a5056218bdd86fb9
Adil <ascodesofficial@gmail.com>
2022-07-22 04:35:13 +0500

    made some new pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <h1>Welcome</h1>
   );
 }
 
