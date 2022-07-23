375f86b6e4a049748abb8ac6284eeaccf3a35b43
Adil <ascodesofficial@gmail.com>
2022-07-23 06:45:36 +0500

    Navigation bar and routing process completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Menu from "../src/pages/Menu";
 import About from "../src/pages/About";
 import Contact from "../src/pages/Contact";
 import NoPage from "../src/pages/NoPage";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 
 function App() {
   return (
