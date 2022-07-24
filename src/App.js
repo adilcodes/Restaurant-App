bda84702ac2ab6561ae34d143ca75f3bf0b66aff
Adil <ascodesofficial@gmail.com>
2022-07-24 19:58:05 +0500

    Footer component Completed

--- src/App.js
+++ src/App.js
@@ -8,20 +8,24 @@ import Contact from "../src/pages/Contact";
 import NoPage from "../src/pages/NoPage";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<NavBar />}>
-          <Route index element={<Home />} />
-          <Route path="menu" element={<Menu />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavBar />}>
+            <Route index element={<Home />} />
+            <Route path="menu" element={<Menu />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-10 mx-auto text-center">
+                <div className="mx-auto mb-2 d-flex justify-content-center align-items-center gap-2">
+                    <a href="https://www.facebook.com/" target="_blank" className="footer-sm-icon d-flex justify-content-center align-items-center">
+                        <i className="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a href="https://www.instagram.com/" target="_blank" className="footer-sm-icon d-flex justify-content-center align-items-center">
+                        <i className="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="https://twitter.com/" target="_blank" className="footer-sm-icon d-flex justify-content-center align-items-center">
+                        <i className="fa-brands fa-twitter"></i>
+                    </a>
+                </div>
+                <p>
+                    © All Rights Reserved | <Link to="/" className="text-decoration-none colored-text">ChillGrill</Link>
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
