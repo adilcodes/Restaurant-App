7195dc5fe4edafbbc1b74e85ffe4b27294b9f219
Adil <ascodesofficial@gmail.com>
2022-07-25 00:41:22 +0500

    Contact Page completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import contactImg from "../images/contactImg.png"
 
 export default function Contact() {
+
+  const [data, setData] = useState(
+    {
+      email : "",
+      phone : "",
+      message : "",
+    }
+  );
+
+  const monitorInput = (event) => {
+    const { name, value } = event.target;
+    setData({...data, [name]:value});
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    alert(
+      `Email : ${data.email}.
+Phone : ${data.phone}.
+Message : ${data.message}.`
+    );
+  };
+
   return (
     <>
       <div className="mt-5">
@@ -15,21 +38,43 @@ export default function Contact() {
                 <img src={contactImg} alt="Contact Us" className="img-fluid" />
               </div>
               <div className="col-md-6 pt-5 pt-lg-0">
-                <form id="form">
+                <form id="form" onSubmit={formSubmit}>
                   <p className="form-label">Email</p>
                   <div className="d-flex align-items-center gap-2">
                     <div className="input-icon d-flex justify-content-center align-items-center">📧</div>
-                    <input type="mail" name="email" className="form-control bg-transparent" placeholder="Enter Email" />
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control bg-transparent"
+                      placeholder="Enter Email"
+                      value={data.email}
+                      onChange={monitorInput}
+                    />
                   </div>
                   <br />
                   <p className="form-label">Phone Number</p>
                   <div className="d-flex align-items-center gap-2">
                     <div className="input-icon d-flex justify-content-center align-items-center">📞</div>
-                    <input type="text" name="phone" className="form-control bg-transparent" placeholder="Enter Phone Number" />
+                    <input
+                      type="text"
+                      name="phone"
+                      className="form-control bg-transparent"
+                      placeholder="Enter Phone Number"
+                      value={data.phone}
+                      onChange={monitorInput}
+                    />
                   </div>
                   <br />
                   <p className="form-label">Your Message</p>
-                  <textarea name="message" cols="30" rows="5" className="form-control bg-transparent" placeholder="Type Your Message..."></textarea>
+                  <textarea
+                    name="message"
+                    cols="30"
+                    rows="5"
+                    className="form-control bg-transparent"
+                    placeholder="Type Your Message..."
+                    value={data.message}
+                    onChange={monitorInput}
+                  ></textarea>
                   <button type="submit" className="btn btn-primary mt-3 px-4" id="send-btn">Send</button>
                 </form>
               </div>
