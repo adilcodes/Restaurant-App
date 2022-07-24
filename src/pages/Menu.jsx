e67ad2176b7f0362992149eeb4e157b7883e83f5
Adil <ascodesofficial@gmail.com>
2022-07-24 21:48:07 +0500

    Search bar Added to menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -5,8 +5,14 @@ import DummyData from "../components/DummyData";
 export default function Menu() {
   return (
     <>
-      <div className="mt-5">
-        <h1 className="text-center">Our Menu</h1>
+      <div className="mt-5 col-10 mx-auto d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-center mb-4">Our Menu</h1>
+        <div className="search-item col-8 d-flex gap-3">
+          <input type="text" className="form-control search-bar" placeholder="Search Food 🍲" />
+          <button className="btn btn-primary rounded-5 text-center" id="send-btn">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
       </div>
       <div className="container-fluid mb-5">
         <div className="row">
