edabb395cb93c57566e29a0b824f481c537fbf50
Adil <ascodesofficial@gmail.com>
2022-07-26 21:04:25 +0500

    Login Page Completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import logo from "../images/logo.jpg"
 
 export default function Login() {
   return (
-    <h1>Hello Login!!!</h1>
+    <>
+      <div id="header" className="d-flex align-items-center login-header">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-10 row mx-auto flex-column justify-content-center align-items-center gap-4">
+              <div className="col-lg-4 col-10 d-flex flex-column justify-content-center align-items-center gap-3">
+                <img src={logo} alt="logo" width="74" />
+                <h2 className="text-center">Seems like you're hungry 🤤 </h2>
+              </div>
+              <div className="col-md-7 col-lg-4 col-10">
+                <form id="auth-form">
+                  <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control bg-transparent auth-field" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email Here" />
+                  </div>
+                  <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control bg-transparent auth-field" id="exampleInputPassword1" placeholder="Enter Password Here" />
+                  </div>
+                  <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                      <label className="form-check-label" for="exampleCheck1">Rememeber me</label>
+                  </div>
+                  <button type="submit" className="btn btn-primary px-4" id="send-btn">Sign in</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
