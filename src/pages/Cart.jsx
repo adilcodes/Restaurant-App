e889da7a24ed0f764b073159f3e34b00acd45f92
Adil <ascodesofficial@gmail.com>
2022-07-27 23:46:49 +0500

    Cart Page completed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CartItem from "../components/CartItem";
+import { Scrollbars } from 'react-custom-scrollbars-2';
 
 export default function Cart() {
   return (
@@ -13,9 +14,22 @@ export default function Cart() {
             </div>
             <div className="row">
                 <div className="col-10 mt-4 mx-auto p-3 bg-white rounded-3 cart-items-container">
-                    <CartItem />
-                    <CartItem />
-                    <CartItem />
+                    <Scrollbars>
+                        <CartItem />
+                        <CartItem />
+                        <CartItem />
+                        <CartItem />
+                        <CartItem />
+                        <CartItem />
+                        <CartItem />
+                        <CartItem />
+                    </Scrollbars>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10 mt-4 mx-auto d-flex flex-column align-items-end">
+                    <h4 className="mb-3"><span className="m-0 p-0 fw-semibold">Cart Total : </span>2000 Rs</h4>
+                    <button className="btn btn-view-item border-1 rounded-0 fs-5 px-4">Checkout</button>
                 </div>
             </div>
         </div>
