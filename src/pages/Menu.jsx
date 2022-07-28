e27fdf414fedb1fbe80bb477a3b726a3de34fd25
Adil <ascodesofficial@gmail.com>
2022-07-29 01:41:03 +0500

    search functionality added successfully

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import FoodCard from "../components/FoodCard";
 import DummyData from "../components/DummyData";
 
 
 export default function Menu() {
 
+  const [searchItem, setSearchItem] = useState("");
+
+  const searchItems = (event) => {
+    setSearchItem(event.target.value);
+  }
+
+  const filteredItems = DummyData.filter((item) => {
+    return (item.foodTitle.toLowerCase().includes(searchItem.toLowerCase()));
+  })
+
   return (
     <>
       <div className="mt-5 col-10 mx-auto d-flex flex-column justify-content-center align-items-center">
         <h1 className="text-center mb-4">Our Menu</h1>
         <div className="search-item col-8 d-flex gap-3">
-          <input type="text" className="form-control search-bar" placeholder="Search Food 🍲" />
-          <button className="btn btn-primary rounded-5 text-center" id="send-btn">
-            <i className="fa-solid fa-magnifying-glass"></i>
-          </button>
+          <input
+            type="text"
+            className="form-control search-bar"
+            placeholder="Search Food 🍲"
+            value={searchItem}
+            onChange={searchItems}
+          />
         </div>
       </div>
       <div className="container-fluid mb-5">
         <div className="row">
           <div className="col-10 row gy-4 mx-auto">
-            {DummyData.map((foodItem) => {
-              return(
-                <FoodCard
-                key={foodItem.foodId}
-                foodItem = {foodItem}      
-                />
-              )
-            })}
+            {filteredItems.length > 0 ? (
+              filteredItems.map((filteredItem) => {
+                return (
+                  <FoodCard
+                    key={filteredItem.foodId}
+                    foodItem={filteredItem}
+                  />
+                )
+              })
+            ) : (
+              <p className="text-center fs-4">Sorry, No match found 😢 </p>
+            )}
           </div>
         </div>
       </div>
