e51214dd1c9e17ea786725add11f0c3c16bb8fb2
Adil <ascodesofficial@gmail.com>
2022-07-29 04:03:43 +0500

    Add the price tag on the food card

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -13,23 +13,26 @@ export default function FoodCard({ foodItem }) {
                     <div className="card-body">
                         <h5 className="card-title">{foodItem.foodTitle}</h5>
                         <p className="card-text">{foodItem.foodText}</p>
-                        {cart.some((item) => item.foodId === foodItem.foodId) ? (
-                            <button
-                                className="btn btn-view-item"
-                                onClick={() => {
-                                    setCart(cart.filter((item) => {
-                                        return item.foodId !== foodItem.foodId;
-                                    }));
-                                }}
-                            >Remove From Cart</button>
-                        ) : (
-                            <button
-                                className="btn btn-view-item"
-                                onClick={() => {
-                                    setCart([...cart, {...foodItem, foodQuantity : 1}]);
-                                }}
-                            >Add To Cart</button>
-                        )}
+                        <div className="p-0 m-0 d-flex flex-wrap justify-content-between align-items-center">
+                            {cart.some((item) => item.foodId === foodItem.foodId) ? (
+                                <button
+                                    className="btn btn-view-item"
+                                    onClick={() => {
+                                        setCart(cart.filter((item) => {
+                                            return item.foodId !== foodItem.foodId;
+                                        }));
+                                    }}
+                                >Remove From Cart</button>
+                            ) : (
+                                <button
+                                    className="btn btn-view-item"
+                                    onClick={() => {
+                                        setCart([...cart, {...foodItem, foodQuantity : 1}]);
+                                    }}
+                                >Add To Cart</button>
+                            )}
+                            <button className="btn border-0">Rs {foodItem.foodPrice}</button>
+                        </div>
                     </div>
                 </div>
             </div>
