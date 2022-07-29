77207888a6cdbb9e66668d10ad1dc47dd21643eb
Adil <ascodesofficial@gmail.com>
2022-07-29 06:25:57 +0500

    collecting the signup details of the user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.jpg";
 
 export default function SignUp() {
 
+  const [details, setDetails] = useState(
+    {
+      name: "",
+      number: "",
+      address: "",
+      email: "",
+      password: "",
+    }
+  );
+
+  const monitorInput = (event) => {
+    const { name, value } = event.target;
+    setDetails({ ...details, [name]: value });
+  }
+
   const formSubmit = (event) => {
     event.preventDefault();
+
+    alert(`
+        name : ${details.name},
+        number : ${details.number},
+        email : ${details.email},
+        address : ${details.address},
+        password : ${details.password},
+    `);
+    
   };
 
   return (
@@ -21,28 +45,69 @@ export default function SignUp() {
                 <form id="auth-form" onSubmit={formSubmit}>
                   {/* Name Field */}
                   <div className="mb-3">
-                    <label for="inputName" className="form-label">Name</label>
-                    <input type="text" className="form-control bg-transparent auth-field" id="inputName" placeholder="Enter your name" />
+                    <label htmlFor="inputName" className="form-label">Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      className="form-control bg-transparent auth-field"
+                      id="inputName"
+                      placeholder="Enter your name"
+                      value={details.name}
+                      onChange={monitorInput}
+                    />
                   </div>
                   {/* Phone Number Field */}
                   <div className="mb-3">
-                    <label for="inputPhone" className="form-label">Phone Number</label>
-                    <input type="tel" className="form-control bg-transparent auth-field" id="inputPhone" placeholder="Enter your number" />
+                    <label htmlFor="inputPhone" className="form-label">Phone Number</label>
+                    <input
+                      type="tel"
+                      name="number"
+                      className="form-control bg-transparent auth-field"
+                      id="inputPhone"
+                      placeholder="Enter your number"
+                      value={details.number}
+                      onChange={monitorInput}
+                    />
                   </div>
                   {/* Address Field */}
                   <div className="mb-3">
-                    <label for="inputAddress" className="form-label">Home Address</label>
-                    <input type="text" className="form-control bg-transparent auth-field" id="inputAddress" placeholder="Enter your address" />
+                    <label htmlFor="inputAddress" className="form-label">Home Address</label>
+                    <input
+                      type="text"
+                      name="address"
+                      className="form-control bg-transparent auth-field"
+                      id="inputAddress"
+                      placeholder="Enter your address"
+                      value={details.address}
+                      onChange={monitorInput}
+                    />
                   </div>
                   {/* Email Field */}
                   <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email Address</label>
-                    <input type="email" className="form-control bg-transparent auth-field" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email" />
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email Address</label>
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control bg-transparent auth-field"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Enter your email"
+                      value={details.email}
+                      onChange={monitorInput}
+                    />
                   </div>
                   {/* Password Field */}
                   <div className="mb-4">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control bg-transparent auth-field" id="exampleInputPassword1" placeholder="Enter your password" />
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      className="form-control bg-transparent auth-field"
+                      id="exampleInputPassword1"
+                      placeholder="Enter your password"
+                      value={details.password}
+                      onChange={monitorInput}
+                    />
                   </div>
                   <button type="submit" className="btn btn-primary px-4" id="send-btn">Sign up</button>
                 </form>
